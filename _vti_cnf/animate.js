1fe091a5e23af202a8b782810f108195e48d3ad6
Anderson Marlon <yagasakiwanderlust@proton.me>
2026-03-13 18:47:24 -0300

    Initial commit

---
+++ _vti_cnf/animate.js
@@ -0,0 +1,11 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|17 Nov 2004 08:51:20 -0000
+vti_extenderversion:SR|4.0.2.2717
+vti_backlinkinfo:VX|primeira\\ hp.htm
+vti_syncwith_ftp.hpg.ig.com.br\:21:TR|17 Nov 2004 06:51:20 -0000
+vti_syncofs_ftp.hpg.ig.com.br\:21:TW|18 Feb 2004 15:09:00 -0000
+vti_syncwith_ftp.hpg.com.br\:21:TR|17 Nov 2004 09:51:20 -0000
+vti_syncofs_ftp.hpg.com.br\:21:TW|21 Feb 2004 01:55:00 -0000
+vti_title:SR|Java 
+vti_syncwith_ftp.planeta.terra.com.br\:21:TR|17 Nov 2004 09:51:20 -0000
+vti_syncofs_ftp.planeta.terra.com.br\:21:TW|07 Mar 2005 22:24:00 -0000
